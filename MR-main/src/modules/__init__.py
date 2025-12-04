334f40e7dd8d3dc5ad435bdd620402462b621d37
AP24110011705 <punyaptejaskumar@gmail.com>
2025-12-04 13:49:55 +0530

    Delete MR-main directory

--- MR-main/src/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-from .engine import recommender_movies
-from .find_by_title import find_by_title
-from .find_user_interests import find_user_interests
-
-__all__ = ["recommender_movies", "find_by_title", "find_user_interests"]
